32eb71988202ea0f87890c30c61607ca38498be2
Trey Simmons <yert511@gmail.com>
2013-10-05 17:04:04 -0500

    Update test include further email format validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -32,7 +32,7 @@ describe User do
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.orb example.user@foo.
-											foo@bar_baz.com foo@bar+baz.com]
+											foo@bar_baz.com foo@bar+baz.com foo@bar..com]
 			addresses.each do |invalid_address|
 				@user.email = invalid_address
 				expect(@user).not_to be_valid
@@ -49,7 +49,17 @@ describe User do
 			end
 		end
 	end
-	
+
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMpl.COm" }
+
+		it "should be saved as all lowercase" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase	
+		end
+	end
+
 	describe "when email address is already taken" do
 		before do 
 			user_with_same_email = @user.dup
