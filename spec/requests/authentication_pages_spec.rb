ea39b9eced01dd8116f8308992b3733ea0fb92c3
Trey Simmons <yert511@gmail.com>
2013-10-06 17:23:12 -0500

    Write test to check for flash.now hash

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -17,7 +17,13 @@ describe "Authentication"  do
 			before { click_button "Sign In" }
 
 			it { should have_title("Sign In") }
-			it { should have_content("div.alert.alert-error", text: "Invalid") }
+			it { should have_selector("div.alert.alert-error", text: "Invalid") }
+
+			describe "after visiting another page" do
+				before { click_link "Home" }
+
+				it { should_not have_selector("div.alert.alert-error") }
+			end
 		end
 
 		describe "with valid information" do
