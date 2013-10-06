651ecbe28a3d5f1291fa37fbf1b44265b7ef630e
Trey Simmons <yert511@gmail.com>
2013-10-06 14:42:07 -0500

    Add tests for signup functionality

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,8 +13,25 @@ describe "UserPages" do
 
 	describe "signup page" do
 		before { visit signup_path }
+		let(:submit) { "Create my account" }
+		
+		describe "with invalid information" do 
+			it "should not create user" do
+				expect { click_button submit }.not_to change(User, :count)
+			end
+		end
 
-		it { should have_content("Sign up") }
-		it { should have_title(full_title("Sign Up")) }
+		describe "with valid information" do
+			before do
+				fill_in "Name",				  with: "Example User"
+				fill_in "Email", 				with: "user@example.com"
+				fill_in "Password", 		with: "secretpassword"
+				fill_in "Confirmation", with: "secretpassword"
+			end
+
+			it "should create a user" do
+				expect { click_button submit }.to change(User, :count).by(1)
+			end
+		end
 	end
 end
