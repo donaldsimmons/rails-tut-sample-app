c28f3a78d82d29046b4f1281b0da7353e2c0db0a
Trey Simmons <yert511@gmail.com>
2013-10-06 17:26:43 -0500

    Include SessionHelper to make module available across controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,13 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-
+		user = User.find_by(email: params[:session][:email].downcase)
+		if user && user.authenticate(params[:session][:password])
+			sign_in user	
+		else
+			flash.now[:error] = "Invalid username/password combination"
+			render 'new'
+		end
 	end
 
 	def destroy
