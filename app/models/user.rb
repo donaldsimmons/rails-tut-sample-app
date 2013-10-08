8c765eb8cf048b2fba39baedb52cd36ba7fc6825
Trey Simmons <yert511@gmail.com>
2013-10-08 08:44:02 -0500

    Fix error in follow! method

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ActiveRecord::Base
 	end
 
 	def follow!(other_user)
-		relationships.create!(:followed_id: other_user.id)
+		relationships.create!(followed_id: other_user.id)
 	end
 
 	def unfollow!(other_user)
