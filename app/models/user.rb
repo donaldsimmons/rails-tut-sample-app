ad1d2f1e0fe4745b0c7f2a5ff4b66927d668a76e
Trey Simmons <yert511@gmail.com>
2013-10-08 08:39:15 -0500

    Add methods for checking the followed users and following users

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,14 @@ class User < ActiveRecord::Base
 		Micropost.where("user_id = ?", id)
 	end
 
+	def following?(other_user)
+		relationships.find_by(followed_id: other_user.id)
+	end
+
+	def follow!(other_user)
+		relationships.create!(:followed_id: other_user.id)
+	end
+
 	def User.new_remember_token
 		SecureRandom.urlsafe_base64
 	end
