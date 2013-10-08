f71580056237d3fe4e93751a6c473b9773b51c93
Trey Simmons <yert511@gmail.com>
2013-10-08 08:26:45 -0500

    Fix error in test

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -18,7 +18,7 @@ describe Relationship do
 
 	describe "when followed id is not present" do
 		before { relationship.followed_id = nil }
-		it { shouuld_not be_valid }
+		it { should_not be_valid }
 	end
 
 	describe "when follower id is not present" do
