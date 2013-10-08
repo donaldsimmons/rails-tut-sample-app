ad43e89f5bf2d13b94ed19d0118b7260bdbe17f0
Trey Simmons <yert511@gmail.com>
2013-10-07 19:16:06 -0500

    Add admin_user callback and method to redirect if current user isn't an admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
  	before_action :signed_in_user, only: [:index, :edit, :update]
 	before_action :correct_user,   only: [:edit, :update]
-	
+	before_action :admin_user, 		 only: :destroy
+
 	def index
 		@users = User.paginate(page: params[:page])
 	end
@@ -58,5 +59,9 @@ class UsersController < ApplicationController
 		def correct_user
 			@user = User.find(params[:id])
 			redirect_to(root_url) unless current_user?(@user)
+		end
+
+		def admin_user
+			redirect_to(root_url) unless current_user.admin?
 		end
 end
