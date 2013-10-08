9f5aaaf894691fdf4ead69dcf41bd2d93d52254b
Trey Simmons <yert511@gmail.com>
2013-10-08 09:50:32 -0500

    Fix error in test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -32,7 +32,7 @@ describe "Static pages" do
 			describe "follower/following counts" do
 				let(:other_user) { FactoryGirl.create(:user) }
 				before do
-					other_user.follow!(:user)
+					other_user.follow!(user)
 					visit root_path
 				end
 
